e8c1ff71281c1823ec645e481e8b647dd8a4688d
harinandana-2 <hharinandana2@gmail.com>
2025-09-22 14:50:11 +0530

    my second commit

--- leap.py
+++ leap.py
@@ -1,5 +1,6 @@
-year=int(input("enter a year: "))
-if (year %4 ==0 and year % 100 !=0) or (year % 400==0) :
-    print(year , "is a leap year")
-else:
-    print(year, "is not a leap year")
+current_year=int(input("enter the current year"))
+final_year=int(input("enter the final year"))
+print(f"leap years from {current_year} to {final_year} are :")
+for year in range (current_year,final_year+1):
+    if (year% 4 ==0 and year % 100!=0) or (year % 400==0):
+     print (year)
